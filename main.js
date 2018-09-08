469f4822de33d8fb9e7e48e275d95b644071e9d9
Aaron-Bird <25508292+Aaron-Bird@users.noreply.github.com>
2018-09-09 04:57:08 +0800

    add merge

--- main.js
+++ main.js
@@ -2,46 +2,46 @@ document.addEventListener('DOMContentLoaded', function () {
 
     let sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-    let createData = amount => {
-        let container = document.querySelector('#sort-view ul');
-        container.innerHTML = '';
-        let ulHeight = parseInt(getComputedStyle(container, null).height);
-        let ulWidth = parseInt(getComputedStyle(container, null).width);
-        let liWidth = ulWidth / amount;
-        let liboardRadius = liWidth / 2;
-
-        let colorStart = 'rgb(51,8,103)'.match(/\d+/g).map(Number);
-        let colorEnd = 'rgb(48,207,208)'.match(/\d+/g).map(Number);
-        let rDifference = (colorEnd[0] - colorStart[0]) / amount;
-        let gDifference = (colorEnd[1] - colorStart[1]) / amount;
-        let bDifference = (colorEnd[2] - colorStart[2]) / amount;
-
-        let arr = [];
-        for (let i = 0; i < amount; i++) {
-            let li = document.createElement('li');
-            let number = (ulHeight - liboardRadius) / amount * i + liboardRadius;
-            li.number = number;
-            li.style.height = number + 'px';
-            li.style.width = liWidth + 'px';
-            li.style.backgroundColor = `rgb(
-                ${Math.floor(colorStart[0] + rDifference * i)},
-                ${Math.floor(colorStart[1] + gDifference * i)},
-                ${Math.floor(colorStart[2] + bDifference * i)}
-            )`;
-            arr.push(li);
-        }
-
-        arr.sort(_ => 0.5 - Math.random());
-        arr.forEach(li => container.appendChild(li));
-    }
-
     let data = {
+        freeze: false,
         container: document.querySelector('#sort-view ul'),
         children: document.querySelector('#sort-view ul').getElementsByTagName('li'),
         speed: parseInt(document.querySelector('#speed').value) || 50,
-        amount: parseInt(document.querySelector('#amount').value) || 40,
         get length() {
             return this.children.length;
+        },
+
+        createData(amount) {
+            let container = document.querySelector('#sort-view ul');
+            container.innerHTML = '';
+            let ulHeight = parseInt(getComputedStyle(container, null).height);
+            let ulWidth = parseInt(getComputedStyle(container, null).width);
+            let liWidth = ulWidth / amount;
+            let liboardRadius = liWidth / 2;
+
+            let colorStart = 'rgb(51,8,103)'.match(/\d+/g).map(Number);
+            let colorEnd = 'rgb(48,207,208)'.match(/\d+/g).map(Number);
+            let rDifference = (colorEnd[0] - colorStart[0]) / amount;
+            let gDifference = (colorEnd[1] - colorStart[1]) / amount;
+            let bDifference = (colorEnd[2] - colorStart[2]) / amount;
+
+            let arr = [];
+            for (let i = 0; i < amount; i++) {
+                let li = document.createElement('li');
+                let number = (ulHeight - liboardRadius) / amount * i + liboardRadius;
+                li.number = number;
+                li.style.height = number + 'px';
+                li.style.width = liWidth + 'px';
+                li.style.backgroundColor = `rgb(
+                    ${Math.floor(colorStart[0] + rDifference * i)},
+                    ${Math.floor(colorStart[1] + gDifference * i)},
+                    ${Math.floor(colorStart[2] + bDifference * i)}
+                )`;
+                arr.push(li);
+            }
+
+            arr.sort(_ => 0.5 - Math.random());
+            arr.forEach(li => container.appendChild(li));
         },
 
         async swap(i, j) {
@@ -72,22 +72,25 @@ document.addEventListener('DOMContentLoaded', function () {
             args.forEach(i => this.children[i].classList.add('sorting'));
 
             await sleep(this.speed);
+        },
+
+        *[Symbol.iterator]() {
+            console.log('start')
+            for (let element of this.children) {
+
+                yield element;
+            }
         }
     };
-    let stop = false;
     data = new Proxy(data, {
         get(target, propKey, receiver) {
-            if (/^\d+$/.test(propKey)) {
-                if (stop) throw 'stop';
-                return target.children[propKey];
+            if (typeof (propKey) !== "symbol" && /^\d+$/.test(propKey)) {
+                if (target.freeze) throw 'stop';
+                return target.children[propKey].number;
             }
             return Reflect.get(target, propKey, receiver);
         },
 
-        // set(target, propKey, value, receiver) {
-        //     if (stop) throw 'stop';
-        //     return Reflect.set(target, propKey, value, receiver);
-        // }
     });
 
     let algorithm = {
@@ -96,7 +99,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 for (let j = 0; j < i - 1; j++) {
                     await data.highlight(j, j + 1);
 
-                    if (data[j].number > data[j + 1].number) {
+                    if (data[j] > data[j + 1]) {
                         await data.swap(j, j + 1);
                     }
                 }
@@ -110,7 +113,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 for (let i = left; i < right; i++) {
                     await data.highlight(i, i + 1);
 
-                    if (data[i].number > data[i + 1].number) {
+                    if (data[i] > data[i + 1]) {
                         await data.swap(i, i + 1);
                     }
 
@@ -119,7 +122,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 for (let i = right; i > left; i--) {
                     await data.highlight(i, i - 1);
 
-                    if (data[i - 1].number > data[i].number) {
+                    if (data[i - 1] > data[i]) {
                         await data.swap(i, i - 1);
                     }
                 }
@@ -133,7 +136,7 @@ document.addEventListener('DOMContentLoaded', function () {
             while (left < right) {
                 let j = left;
                 for (let i = left; i <= right; i++) {
-                    if (data[j].number > data[i].number) {
+                    if (data[j] > data[i]) {
                         j = i;
                     }
 
@@ -146,7 +149,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 j = right;
                 for (let i = right; i >= left; i--) {
-                    if (data[i].number > data[j].number) {
+                    if (data[i] > data[j]) {
                         j = i;
                     }
 
@@ -164,7 +167,7 @@ document.addEventListener('DOMContentLoaded', function () {
             for (let i = start + 1; i <= end; i++) {
                 await data.highlight(p, i);
 
-                if (data[i].number < data[p].number) {
+                if (data[i] < data[p]) {
                     // await data.insert(i, start);
                     await data.insert(i, p);
                     p++;
@@ -185,7 +188,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     k = i;
                 await data.highlight(j, k);
 
-                while (j >= 0 && data[k].number < data[j].number) {
+                while (j >= 0 && data[k] < data[j]) {
                     await data.highlight(j, k);
 
                     await data.insert(k, j);
@@ -205,7 +208,7 @@ document.addEventListener('DOMContentLoaded', function () {
                             pre = j - gap;
                         await data.highlight(pre, cur);
 
-                        while (pre >= 0 && data[pre].number > data[cur].number) {
+                        while (pre >= 0 && data[pre] > data[cur]) {
                             await data.highlight(pre, cur);
 
                             await data.swap(cur, pre);
@@ -215,6 +218,25 @@ document.addEventListener('DOMContentLoaded', function () {
                     }
                 }
                 gap = Math.floor(gap / 2);
+            }
+        },
+
+        async merge(left = 0, right = data.length - 1) {
+            if (left === right) return;
+
+            let middle = left + Math.floor((right - left) / 2);
+            await this.merge(left, middle);
+            await this.merge(middle + 1, right);
+
+            middle++;
+            while (left <= middle && middle <= right) {
+                await data.highlight(left, middle);
+
+                if (data[left] > data[middle]) {
+                    await data.insert(middle, left);
+                    middle++;
+                }
+                left++;
             }
         }
     };
@@ -236,13 +258,13 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         }
 
-        createData(40);
+        data.createData(40);
 
         document.querySelectorAll('.sort').forEach(element => {
             element.addEventListener('click', event => {
                 disableAlgorithmButtonList();
                 var btnName = event.target.id;
-                stop = false;
+                data.freeze = false;
                 algorithm[btnName]().then(function (v) {
                     data.highlight();
                 }).catch(error => {
@@ -254,12 +276,12 @@ document.addEventListener('DOMContentLoaded', function () {
         })
 
         document.querySelector('#stop').addEventListener('click', event => {
-            stop = true;
+            data.freeze = true;
         });
 
         document.querySelector('#shuffle').addEventListener('click', event => {
-            stop = true;
-            createData(data.amount);
+            data.freeze = true;
+            data.createData(data.length);
         });
 
         document.querySelector('#speed').addEventListener('input', event => {
@@ -267,9 +289,8 @@ document.addEventListener('DOMContentLoaded', function () {
         });
 
         document.querySelector('#amount').addEventListener('input', event => {
-            amount = parseInt(event.currentTarget.value);
-            data.amount = amount > 1000 ? 1000 : amount;
-            createData(data.amount);
+            let length = parseInt(event.currentTarget.value);
+            data.createData(length > 800 ? 800 : length);
         });
     }
     init();
